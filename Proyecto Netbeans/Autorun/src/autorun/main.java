9aea991b7305af08507f973d136bc2998ea10d20
TheJaBu <134842754+TheJaBu@users.noreply.github.com>
2024-06-08 15:10:02 +0200

    Add files via upload

---
+++ Proyecto Netbeans/Autorun/src/autorun/main.java
@@ -0,0 +1,17 @@
+package autorun;
+
+/**
+ *
+ * @author Jaime
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Autorun a = new Autorun();
+        a.setVisible(true);
+    }
+    
+}
